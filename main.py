c2a3685de203e4778fec6fc9da314987840cec98
宇之少 <h147com2022@163.com>
2026-05-23 16:18:42 +0800

    update 优化了前后端交互

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from fastapi.responses import FileResponse
 import os
 import uuid
 from pydantic import BaseModel
+from fastapi.staticfiles import StaticFiles
 
 class DownloadRequest(BaseModel):
     url: str
 
 
 app = FastAPI()
+app.mount("/static",StaticFiles(directory="static"))
 
 save_path = "downloads"
 os.makedirs(save_path, exist_ok=True)
@@ -17,11 +19,11 @@ os.makedirs(save_path, exist_ok=True)
 @app.post("/api/download")
 def download(item: DownloadRequest):
     url = item.url
-    return {"url": url}
+    return {"url": url,"message":"成功"}
 
 @app.get("/")
 def read_root():
-    return FileResponse("index.html", media_type="text/html")
+    return FileResponse("static/index.html", media_type="text/html")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
